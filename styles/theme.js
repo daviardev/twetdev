3a5af2bb990e136d1bb2617655644eef548824c5
devsilva <jersonsilva99@hotmail.com>
2022-10-21 19:03:57 -0500

    style: Create fonts base, colors and breakpoint for the mobile only

---
+++ styles/theme.js
@@ -0,0 +1,12 @@
+export const fonts = {
+    base: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif'
+}
+
+export const colors = {
+    primary: '#0099ff',
+    secondary: '#1c5480'
+}
+
+export const breakpoint = {
+    mobile: '520px'
+}
