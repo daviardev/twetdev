9d82cfbe1ba880cf6312ca49ea3c4534d776b106
devsilva <jersonsilva99@hotmail.com>
2022-10-22 23:24:13 -0500

    feat: Content global styles

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import AppLayout from '../components/AppLayout'
+
+const App = ({ Component, pageProps }) => {
+    return (
+        <AppLayout>
+            <Component { ...pageProps } />
+        </AppLayout>
+    )
+}
+
+export default App
