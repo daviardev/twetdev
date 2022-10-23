dbef70185018ed51dbc2abcd595ff962f89e73db
devsilva <jersonsilva99@hotmail.com>
2022-10-22 23:21:56 -0500

    feat: Create component button

---
+++ components/Button/index.js
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+const Button = ({ children, onClick }) => {
+    return (
+        <>
+            <button className={styles.button} onClick={onClick}>
+                { children }
+            </button>
+        </>
+    )
+}
+
+export default Button
