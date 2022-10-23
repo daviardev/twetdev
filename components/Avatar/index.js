60cdda3500c803a7e09cf68701c4f7038d31221c
devsilva <jersonsilva99@hotmail.com>
2022-10-22 23:21:00 -0500

    feat: Create Avatar component, content img and text

---
+++ components/Avatar/index.js
@@ -0,0 +1,12 @@
+import styles from './styles.module.css'
+
+const Avatar = ({ alt, src, text }) => {
+    return (
+        <div className={styles.content}>
+            <img className={styles.avatar} alt={alt} src={src} title={alt} />
+            { text && <strong>{ text }</strong> }
+        </div>
+    )
+}
+
+export default Avatar
