6f6a4f76d750f06581722c35b78374971d13fbbe
devsilva <jersonsilva99@hotmail.com>
2022-10-22 23:23:53 -0500

    refactor: Content funtionality of the login

--- firebase/client.js
+++ firebase/client.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app'
+import * as firebase from 'firebase'
 
 const firebaseConfig = {
     apiKey: "AIzaSyC66mziN8NIuoxUD_W-_kS8FRukpmQ4bcA",
@@ -8,6 +8,30 @@ const firebaseConfig = {
     messagingSenderId: "1052934596057",
     appId: "1:1052934596057:web:547679ffdb29314d7625c8",
     measurementId: "G-MCMVCZB9SJ"
-  }
+}
 
-export const app = initializeApp(firebaseConfig)
+!firebase.apps.length && firebase.initializeApp(firebaseConfig)
+
+const mapUserFromFirebaseAuth = (user) => {
+  const { displayName, email, photoURL } = user
+
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email
+  }
+}
+
+export const onAuthStateChanged = (onChange) => {
+  return firebase.auth().onAuthStateChanged(user => {
+    const normalizedUser = user ? 
+    mapUserFromFirebaseAuth(user) : null
+    onChange(normalizedUser)
+  })
+}
+
+export const loginWithGithub = () => {
+  const githubProvider = new firebase.auth.GithubAuthProvider()
+
+  return firebase.auth().signInWithPopup(githubProvider).then(mapUserFromFirebaseAuth)
+}
