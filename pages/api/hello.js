9e32bc9cfa1c6967237015044e1a150820da8e8b
devsilva <jersonsilva99@hotmail.com>
2022-10-22 23:19:26 -0500

    remove: Remove another things

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(__, res) {
-  res.status(200).json({
-    userName: 'Goku',
-    love: 'Hago este llamado para que tu vuelvas',
-    isOver: 'Es más de 9000'
-  })
-}
