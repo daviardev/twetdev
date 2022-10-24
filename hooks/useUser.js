b5ca5db4a3825c890a010207f26d9ecda1abcec5
devsilva <jersonsilva99@hotmail.com>
2022-10-24 17:56:06 -0500

    feat: Create a custom hook for the state of the user

---
+++ hooks/useUser.js
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import { onAuthStateChanged } from '../firebase/client'
+
+export const STATES = {
+    NOT_LOGGED: null,
+    NOT_KNOWN: undefined
+}
+
+const useUser = () => {
+    const [user, setUser] = useState(STATES.NOT_KNOWN)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        onAuthStateChanged(setUser)
+    }, [])
+
+    useEffect(() => {
+        user === STATES.NOT_LOGGED && router.push('/')
+    }, [user])
+
+    return user
+}
+
+export default useUser
