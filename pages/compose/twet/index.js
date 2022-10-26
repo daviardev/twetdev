fe8414c407bf70175ea88aa1076915fd188aec29
devsilva <jersonsilva99@hotmail.com>
2022-10-25 19:56:12 -0500

    feat: Add drag image and show image of the user

--- pages/compose/twet/index.js
+++ pages/compose/twet/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
-import { useState } from 'react'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
-import Button from '../../../components/Button'
 import useUser from '../../../hooks/useUser'
+import Button from '../../../components/Button'
+import Avatar from '../../../components/Avatar'
 
-import { addTwet } from '../../../firebase/client'
+import { addTwet, uploadImage } from '../../../firebase/client'
 
 import styles from './styles.module.css'
 
@@ -16,12 +17,37 @@ const COMPOSE_STATES = {
     ERROR: -1,
 }
 
+const DRAG_IMAGE_STATES = {
+    ERROR: -1,
+    NONE: 0,
+    DRAG_OVER: 1,
+    UPLOADING: 2,
+    COMPLETE: 3
+}
+
 const ComposeTwet = () => {
     const [message, setMessage] = useState('')
     const [status, setStatus] = useState(COMPOSE_STATES.USER_NOT_KNOWN)
 
+    const [drag, setDrag] = useState(DRAG_IMAGE_STATES.NONE)
+    const [task, setTask] = useState(null)
+    const [imgURL, setImgURL] = useState(null)
+
     const user = useUser()
     const router = useRouter()
+
+    useEffect(() => {
+        if (task) {
+            const onProgress = () => { }
+            const onError = () => { }
+            const onComplete = () => {
+                console.log('onComplete')
+                task.snapshot.ref.getDownloadURL().then(setImgURL)
+            }
+
+            task.on('state_changed', onProgress, onError, onComplete)
+        }
+    }, [task])
 
     const handleChange = (event) => {
         const { value } = event.target
@@ -35,7 +61,8 @@ const ComposeTwet = () => {
             avatar: user.avatar,
             content: message,
             userId: user.uid,
-            username: user.username
+            username: user.username,
+            img: imgURL
         })
             .then(() => {
                 router.push('/home')
@@ -45,6 +72,27 @@ const ComposeTwet = () => {
                 setStatus(COMPOSE_STATES.ERROR)
             })
     }
+
+    const handleDragEnter = e => {
+        e.preventDefault()
+        setDrag(DRAG_IMAGE_STATES.DRAG_OVER)
+    }
+
+    const handleDragLeave = e => {
+        e.preventDefault()
+        setDrag(DRAG_IMAGE_STATES.NONE)
+    }
+
+    const handleDrop = e => {
+        e.preventDefault()
+        setDrag(DRAG_IMAGE_STATES.NONE)
+        const file = e.dataTransfer.files[0]
+        console.log()
+
+        const task = uploadImage(file)
+        setTask(task)
+    }
+
     const isButtonDisabled = !message.length || status === COMPOSE_STATES.LOADING
 
     return <>
@@ -52,12 +100,37 @@ const ComposeTwet = () => {
             <title>Crear un twet | twetdev</title>
             <link rel='icon' href='/logo-dev.png' />
         </Head>
-        <form onSubmit={handleSubmit}>
-            <textarea className={styles.writeText} onChange={handleChange} value={message} placeholder='¿Qué está pasando?'></textarea>
-            <div className={styles.content}>
-                <Button disabled={isButtonDisabled}>Twittear</Button>
-            </div>
-        </form>
+        <section className={styles.form_container}>
+            {user && (
+                <section className={styles.avatar_container}>
+                    <Avatar src={user.avatar} />
+                </section>
+            )}
+            <form onSubmit={handleSubmit}>
+                <textarea className={styles.writeText}
+                    value={message}
+                    onDrop={handleDrop}
+                    onChange={handleChange}
+                    onDragEnter={handleDragEnter}
+                    onDragLeave={handleDragLeave}
+                    placeholder='¿Qué está pasando?'
+                ></textarea>
+                {imgURL && (
+                    <section className={styles.remove_img}>
+                        <button className={styles.button} onClick={() => setImgURL(null)}>x</button>
+                        <img className={styles.image} src={imgURL} />
+                    </section>
+                )}
+                <div className={styles.content}>
+                    <Button disabled={isButtonDisabled}>Twittear</Button>
+                </div>
+            </form>
+        </section>
+        <style jsx>{`
+            textarea {
+                border: ${drag === DRAG_IMAGE_STATES.DRAG_OVER ? '3px dashed #09f' : '3px solid transparent'};
+            }
+        `}</style>
     </>
 }
 
