ea109d4bd6375f105120e87939447569064ce9d3
devsilva <jerson.silva99@hotmail.com>
2022-10-28 18:45:27 -0500

    refactor: Remove path

--- components/Twet/index.js
+++ components/Twet/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Avatar from '../Avatar'
+import Avatar from 'components/Avatar'
 import useTimeAgo from 'hooks/useTimeAgo'
 
 import styles from './styles.module.css'
