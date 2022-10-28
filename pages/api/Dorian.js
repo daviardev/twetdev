8eeccc54d95b9ed61ad5235cac6615f8e38d7ce3
devsilva <jerson.silva99@hotmail.com>
2022-10-28 18:40:36 -0500

    remove: Remove test of the api

--- pages/api/Dorian.js
+++
@@ -1,5 +0,0 @@
-export default (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'Application/json')
-    res.end(JSON.stringify({ name: 'Dorian, quien mundá es Dorian' }))
-}
