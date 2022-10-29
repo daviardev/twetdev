0d92c537859958be899a6068b5b1db611e6ef9af
devsilva <jerson.silva99@hotmail.com>
2022-10-28 19:10:13 -0500

    refactor: Redirect to twit, and add effect

--- components/Twet/index.js
+++ components/Twet/index.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import Avatar from 'components/Avatar'
 import useTimeAgo from 'hooks/useTimeAgo'
+
 
 import styles from './styles.module.css'
 
@@ -14,17 +17,26 @@ const Twet = ({
   createdAt
 }) => {
   const timeago = useTimeAgo(createdAt)
+  const router = useRouter()
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    router.push(`/status/${id}`)
+  }
 
   return <>
-    <article className={styles.article}>
+    <article className={styles.article} onClick={handleClick}>
       <div className={styles.avatar_content}>
         <Avatar alt={username} src={avatar} />
       </div>
       <section>
         <header>
           <strong>{username}</strong>
-          <span className={styles.dot}>·</span>
-          <time className={styles.date}>{timeago}</time>
+          <span className={styles.dot}> · </span>
+          <Link href={`/status/${id}`}>
+            <time className={styles.date}>{timeago}</time>
+          </Link>
+
         </header>
         <p className={styles.paragraph}>{content}</p>
         {img && <img className={styles.image} src={img} />}
