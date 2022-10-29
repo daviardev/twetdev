6557e9ea447f32c25eb1a56355d0bb37487517ed
devsilva <jerson.silva99@hotmail.com>
2022-10-29 15:38:10 -0500

    refactor: Add spinner fot the loading and get static path

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,23 +1,49 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+
+import { firestore } from 'firebase/admin'
 
 import Twet from 'components/Twet'
 
 export default function TwitPage(props) {
+    const router = useRouter()
+
+    if (router.isFallback) return <img src='/spinner.gif' />
+
     return <>
         <Twet {...props} />
     </>
 }
 
-TwitPage.getInitialProps = (context) => {
-    const { query, res } = context
-    const { id } = query
+export async function getStaticPaths() {
+    return {
+        paths: [{ params: { id: '7WBbJyquh2HD9WvCEMTD' } }],
+        fallback: true
+    }
+}
 
-    return fetch(`http://localhost:3000/api/twits/${id}`).then(
-        (apiRes) => {
-            if (apiRes.ok) return apiRes.json()
-            if (res) {
-                res.writeHead(301, { location: '/home' }).end()
+export async function getStaticProps(context) {
+    const { params } = context
+    const { id } = params
+
+    return firestore
+        .collection('twits')
+        .doc(id)
+        .get()
+        .then((doc) => {
+            const id = doc.id
+            const data = doc.data()
+            const { createdAt } = data
+
+            const props = {
+                id,
+                ...data,
+                createdAt: +createdAt.toDate()
             }
-        }
-    )
+
+            return { props }
+
+        }).catch(() => {
+            return { props: {} }
+        })
 }
