98519c1ced9d952a8a7318cf2901cdef92087b9e
devsilva <jerson.silva99@hotmail.com>
2022-10-28 22:53:17 -0500

    feat: Create component NavBar

---
+++ components/NavBar/index.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+
+import Home from 'components/Icons/Home'
+import Create from 'components/Icons/Create'
+import Search from 'components/Icons/Search'
+
+import styles from './styles.module.css'
+
+const NavBar = () => {
+    return <>
+        <nav className={styles.navbar}>
+            <Link href='/home'>
+                <a>
+                    <Home width={32} height={32} stroke="#000000" />
+                </a>
+            </Link>
+            <Link href='/compose/twet'>
+                <a>
+                    <Create width={32} height={32} stroke="#000000" />
+                </a>
+            </Link>
+            <Link href='/account'>
+                <a>
+                    <Search width={32} height={32} stroke="#000000" />
+                </a>
+            </Link>
+        </nav>
+    </>
+}
+
+export default NavBar
