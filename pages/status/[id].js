eb4b5e86180e74497706040708d514b1fe624fb6
devsilva <jerson.silva99@hotmail.com>
2022-10-28 19:11:48 -0500

    feat: Page of the twit

---
+++ pages/status/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TwitPage = () => {
+    return <>
+        <h1>EYYYY muy buenas a todos guapísimos aqui vegetta 777 en una play directo de maicra</h1>
+    </>
+}
+
+export default TwitPage
