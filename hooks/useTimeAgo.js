3500bfcea7044df3fe72187a5c2708894bd13de4
devsilva <jerson.silva99@hotmail.com>
2022-10-28 23:23:46 -0500

    refactor: Import formateDate and language

--- hooks/useTimeAgo.js
+++ hooks/useTimeAgo.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
+
+import { formatDate } from './useDateTimeFormat'
+import { DEFAULT_LANGUAGE } from 'contants/locale'
+
+const isRelativeTimeFormatSupported = typeof Intl !== 'undefined' && Intl.RelativeTimeFormat
 
 const DATE_UNITS = [
-    ['year', 31536000],
-    ['month', 2592000],
     ['day', 86400],
     ['hour', 3600],
+    ['year', 31536000],
+    ['month', 2592000],
     ['minute', 60],
     ['seconds', 1]
 ]
 
-const getDateDiffs = timestamp => {
+const getDateDiffs = (timestamp) => {
     const now = Date.now()
     const elapsed = (timestamp - now) / 1000
 
     for (const [unit, secondsInUnit] of DATE_UNITS) {
         if (Math.abs(elapsed) > secondsInUnit || unit === 'seconds') {
             const value = Math.round(elapsed / secondsInUnit)
+
             return { value, unit }
         }
     }
@@ -25,15 +31,21 @@ const useTimeAgo = (timestamp) => {
     const [timeago, setTimeago] = useState(() => getDateDiffs(timestamp))
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            const newTimeAgo = getDateDiffs(timestamp)
-            setTimeago(newTimeAgo)
-        }, 5000)
-        return () => clearInterval(interval)
+        if (isRelativeTimeFormatSupported) {
+            const interval = setInterval(() => {
+                const newTimeAgo = getDateDiffs(timestamp)
+                setTimeago(newTimeAgo)
+            }, 5000)
+            return () => clearInterval(interval)
+        }
     }, [timestamp])
 
-    const rtf = new Intl.RelativeTimeFormat('es', { style: 'short' })
+    if (!isRelativeTimeFormatSupported) {
+        formatDate(timestamp)
+    }
+
     const { value, unit } = timeago
+    const rtf = new Intl.RelativeTimeFormat(DEFAULT_LANGUAGE, { style: 'long' })
 
     return rtf.format(value, unit)
 }
